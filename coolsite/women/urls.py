6aafe399c939d864c09ac8fe001ecebcd788a6c5
Viktor1732 <programmer.kras@gmail.com>
2022-09-14 23:56:09 +0700

    - добавил функции к блоку меню.  -прописал url-адреса в шаблонах.

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -5,8 +5,12 @@ from coolsite import settings
 from .views import *
 
 urlpatterns = [
-    path('', index),
-    path('about/', about),
+    path('', index, name='home'),
+    path('about/', about, name='about'),
+    path('addpage/', addpage, name='add_page'),
+    path('contact/', contact, name='contact'),
+    path('login/', login, name='login'),
+    path('post/<int:post_id>/', show_post, name='post')
 ]
 
 if settings.DEBUG:
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,19 +1,44 @@
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound, HttpResponse
 from django.shortcuts import render
 
 from .models import *
 
-menu = ['О сайте', 'Добавить статью', 'Обратная связь', 'Войти']
+menu = [{'title': 'О сайте', 'url_name': 'about'},
+        {'title': 'Добавить статью', 'url_name': 'add_page'},
+        {'title': 'Обратная связье', 'url_name': 'contact'},
+        {'title': 'Войти', 'url_name': 'login'},
+        ]
 
 
 def index(request):
     posts = Women.objects.all()
-    return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Famous women'})
+    context = {
+        'posts': posts,
+        'menu': menu,
+        'title': 'Главная страница'
+    }
+    return render(request, 'women/index.html', context=context)
 
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Ошибка 404. Страница не найдена...</h1>')
 
 
+def addpage(request):
+    return HttpResponse('Добавить статьи')
+
+
+def contact(request):
+    return HttpResponse('Обратная связь')
+
+
+def login(request):
+    return HttpResponse('Авторизация')
+
+
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'Famous women'})
+
+
+def show_post(request, post_id):
+    return HttpResponse(f'Отображение статьи с id: {post_id}')
