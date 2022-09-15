9b67c425f04dc518f8438c4078af046471e2e60e
Viktor1732 <programmer.kras@gmail.com>
2022-09-15 22:03:43 +0700

    - Добавил и соеденил твблицу по внешнему ключу. - Прописал к статьям категории.

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -1,3 +1,4 @@
+# Прописываем все модели для взаимодействия с БД
 from django.db import models
 from django.urls import reverse
 
@@ -9,10 +10,22 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True) #id к cat - django добавит автоматически
+
 
     def __str__(self): #Для взаимодействия модели с базой данных через python manage.py shell
         return self.title
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={'cat_id': self.pk})
 
