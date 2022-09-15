8990f16664ce93cde07182bf9a433130e45cc9ae
Viktor1732 <programmer.kras@gmail.com>
2022-09-16 00:32:45 +0700

    - Добави статью про Андриану Гранд. - В админ-панель добавил фильтр для плей. - В index.html добавлено отображение даты моздания статьи и категории. - В index.html добавлена возможность загружать фото.

--- coolsite/women/admin.py
+++ coolsite/women/admin.py
@@ -7,6 +7,8 @@ class WomenAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'time_create', 'photo', 'is_published')  # эти поля будут отображаться в админ-панели
     list_display_links = ('id', 'title')  # поля на которые мы можем кликнуть и перейти для редактирования
     search_fields = ('title', 'content')  # по каким полям можно будет делать поиск
+    list_editable = ('is_published',)  # список полей, которые будут редактируемые
+    list_filter = ('is_published', 'time_create')  # поля по которым сможе фильтровать список статей
 
 
 class CategoryAdmin(admin.ModelAdmin):
