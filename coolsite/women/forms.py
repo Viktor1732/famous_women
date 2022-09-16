c70e70862cac4dc7d684aa85120abcca3f76fd02
Viktor1732 <programmer.kras@gmail.com>
2022-09-16 23:54:30 +0700

    - Добавлен класс для title в forms - В views прописано добавление записи в БД. - В add_page добавлен цикл for для уменьшения кода.

--- coolsite/women/forms.py
+++ coolsite/women/forms.py
@@ -4,7 +4,7 @@ from .models import *
 
 
 class AddPostForm(forms.Form):  # Прописывае только те данные которые необходимы конечному пользователю.
-    title = forms.CharField(max_length=255, label='Заголовок')
+    title = forms.CharField(max_length=255, label='Заголовок', widget=forms.TextInput(attrs={'class': 'form-input'}))
     slug = forms.SlugField(max_length=255, label='URL')
     content = forms.CharField(widget=forms.Textarea(attrs={'cols': 60, 'rows': 10}), label='Контент')
     is_published = forms.BooleanField(label='Публикация', required=False,
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseNotFound, HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import AddPostForm
 from .models import *
@@ -31,7 +31,12 @@ def addpage(request):
     if request.method == 'POST':
         form = AddPostForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
+            try:
+                Women.objects.create(**form.cleaned_data)
+                return redirect('home')
+            except:
+                form.add_error(None, 'Ошибка добавления поста')
+
     else:
         form = AddPostForm()
     return render(request, 'women/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
