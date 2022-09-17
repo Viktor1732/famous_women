3ca5250f8734c5e951977cbb5bd1ae8362b3de97
Viktor1732 <programmer.kras@gmail.com>
2022-09-17 13:58:08 +0700

    - Добавил поле загрузки изображений.

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -7,7 +7,7 @@ class Women(models.Model):
     title = models.CharField(max_length=255,
                              verbose_name='ЗАГОЛОВОК')  # verbose-name - название для заголовка в админ-панели.
     slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
-    content = models.TextField(blank=True, verbose_name='НАЗВАНИЕ СТАТЬИ')
+    content = models.TextField(blank=True, verbose_name='НАЗВАНИЕ СТАТЬИ') # blank=True - необязательно для заполнения
     photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
